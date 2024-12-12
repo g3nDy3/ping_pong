99596ad1b22061c5a788a06c57a223a7827ee74c
g3nDy3 <kekrtr123@gmail.com>
2024-12-12 19:37:56 +0300

    part_1 v1.1

--- ping_pong.py
+++ ping_pong.py
@@ -27,18 +27,19 @@ class GameSprite(sprite.Sprite):
 
 #класс-наследник для спрайта-игрока (управляется стрелками)
 class Player(GameSprite):
-    def update_r(self):
-        keys = key.get_pressed()
-        if keys[K_UP] and self.rect.y > 5:
-            self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.y < win_height - 80:
-            self.rect.y += self.speed
     def update_l(self):
         keys = key.get_pressed()
         if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
         if keys[K_s] and self.rect.y < win_height - 80:
             self.rect.y += self.speed
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+
 
 #флаги отвечающие за состояние игры
 game = True
