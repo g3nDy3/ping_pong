cfe9387d93a62adfc1fd4709d18e3c9e0ddecce9
g3nDy3 <kekrtr123@gmail.com>
2025-05-23 17:44:02 +0300

    Update ping_pong.py

--- ping_pong.py
+++ ping_pong.py
@@ -25,27 +25,6 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
-#класс-наследник для спрайта-игрока (управляется стрелками)
-class Player(GameSprite):
-    def update_r(self):
-        keys = key.get_pressed()
-        if keys[K_UP] and self.rect.y > 5:
-            self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.y < win_height - 80:
-            self.rect.y += self.speed
-    def update_l(self):
-        keys = key.get_pressed()
-        if keys[K_w] and self.rect.y > 5:
-            self.rect.y -= self.speed
-        if keys[K_s] and self.rect.y < win_height - 80:
-            self.rect.y += self.speed
-
-
-
-#флаги отвечающие за состояние игры
-game = True
-clock = time.Clock()
-FPS = 60
 
 #создания мяча и ракетки    
 racket1 = Player('racket.png', 30, 200, 4, 50, 150) # при созданни спрайта добавляется еще два параметра
@@ -67,38 +46,5 @@ while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-
-            
-    if finish != True:
-        window.fill(back)
-        racket1.update_l()
-        racket2.update_r()
-        ball.rect.x += speed_x
-        ball.rect.y += speed_y
-
-        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
-            speed_x *= -1
-            speed_y *= 1
-        
-        # если мяч достигает границ экрана меняем направление его движения
-        if ball.rect.y > win_height-50 or ball.rect.y < 0:
-            speed_y *= -1
-
-        # если мяч улетел дальше ракетки, выводим условие проигрыша для первого игрока
-        if ball.rect.x < 0:
-            finish = True
-            window.blit(lose1, (200, 200))
-            game_over = True
-
-        # если мяч улетел дальше ракетки, выводим условие проигрыша для второго игрока
-        if ball.rect.x > win_width:
-            finish = True
-            window.blit(lose2, (200, 200))
-            game_over = True
-
-        racket1.reset()
-        racket2.reset()
-        ball.reset()
-
     display.update()
     clock.tick(FPS)
