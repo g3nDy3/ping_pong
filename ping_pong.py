3aee5e3f7031afd31ef74f7ef83c7c703751a73c
g3nDy3 <kekrtr123@gmail.com>
2025-05-23 18:17:48 +0300

    Update ping_pong.py

--- ping_pong.py
+++ ping_pong.py
@@ -25,6 +25,22 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
+#класс-наследник для спрайта-игрока (управляется стрелками)
+class Player(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+
 
 #создания мяча и ракетки    
 racket1 = Player('racket.png', 30, 200, 4, 50, 150) # при созданни спрайта добавляется еще два параметра
@@ -46,5 +62,15 @@ while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+    
+    if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
+
     display.update()
     clock.tick(FPS)
